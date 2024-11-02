b697c43ac1a19a63749fa8ecf0dd61c190e79e06
davelin18yufan <davelin30630@gmail.com>
2024-11-02 15:21:54 +0800

    [refactor] adjust config and extract out routes as an independant file.

---
+++ components/dashboard/index.tsx
@@ -0,0 +1,19 @@
+import DatePicker from "./DatePicker";
+import FundPieChart from "./FundPieChart";
+import RealizedPnlChart from "./RealizedPnlChart";
+import ProfitChart from "./ProfitChart";
+import TradeFundBase from "./TradeFundBase";
+import TradeLog from "./TradeLog";
+import TradePlan from "./TradePlan";
+import TradeSummary from "./TradeSummary";
+
+export {
+  DatePicker,
+  FundPieChart,
+  RealizedPnlChart,
+  ProfitChart,
+  TradeFundBase,
+  TradeLog,
+  TradePlan,
+  TradeSummary,
+}
